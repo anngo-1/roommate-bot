3f1dd45f542a655502fef84f68637659fbbfe91c
An Ngo <andngo2004@gmail.com>
2022-09-22 22:05:43 -0700

    command functionality

--- botfunc.py
+++ botfunc.py
@@ -1,12 +1,62 @@
 
-def registerschedule():
-    print(None)
 
-def schedule(name):
-    print(None)
+class Schedules:
+    def __init__(self):
+        self.name = "Schedules"
+        self.desc = "This is a tool to upload & view class schedules of roommates."
+        funclist = [method_name for method_name in dir(Schedules) if callable(getattr(Schedules, method_name)) if method_name[0]!="_"]
+        self.functions = ""
+        for i in funclist:
+            self.functions = "!" + i + "\n" + self.functions
+    def __str__(self):  
+        return "class working"
 
-def registerreminder():
-    print(None)
+    def registerschedule(self):
+        print("registerschedule working")
 
-def reminders():
-    print(None)
+    def schedule(self, name):
+        print("scheduleworking")
+
+
+class Reminders:
+    def __init__(self):
+        self.name = "Reminders"
+        self.desc = "Reminders allows Roommate bot to send notifications ahead of time to your roommates to remind them of events, private time, etc."
+        funclist = [method_name for method_name in dir(Reminders) if callable(getattr(Reminders, method_name)) if method_name[0]!="_"]
+        self.functions = ""
+        for i in funclist:
+            self.functions = "!" + i + "\n" + self.functions
+    
+
+
+    def __str__(self):  
+        return "class working"
+
+    def registerreminder(self, date, time, text):
+        
+        if date == None or time == None or text == None:
+            return  "Error: function received the incorrect arguments \n``Example: !registerreminder 10/25 10:23pm I need to beat it``"
+
+        else:
+            return (date + " "  +time+ " " + text)
+
+    def reminders(self):
+        print("None")
+
+
+async def messagefunc(client, chan, text):
+    await chan.send(text) 
+
+
+
+
+
+
+
+
+
+
+scheduleobj = Schedules()
+remindersobj = Reminders()
+
+commandlist = [scheduleobj, remindersobj]
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # importing 
-from inspect import getmembers, isfunction
-
 import botfunc
+from botfunc import *
 import discord
 import os
 from dotenv import load_dotenv
@@ -17,16 +16,13 @@ intents.message_content = True
 
 
 #command list comprehension
-functuple = getmembers(botfunc, isfunction)
-commandlist = [i[0] for i in functuple]
-print (commandlist)
 
 
 
 @client.event
 async def on_ready():
     #status for commands
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='!commands'))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='!functions'))
     #checks
     print("bot ready")
 
@@ -36,17 +32,42 @@ async def on_ready():
 async def on_message(message):
     check = message.author.id != client.user.id
 
-    if message.content.startswith("!commands") and check:
+    for i in commandlist:
+        if message.content[1:message.content.find(" ")] in i.functions:
+            if message.content.find(" ") == -1:
+                func = getattr(i, message.content[1:])
+            else:
+                func = getattr(i, message.content[1:message.content.find(" ")])
+            argslist = message.content.split(" ")
+            x = None
+            y= None
+            z= None
+      
+            try: 
+                x = argslist[1]
+                y = argslist[2]
+                z = " ".join(argslist[3:])
+            except:
+                    
+                print(None)
+                    
+                                    
+          
+           
+            await message.channel.send(func(x, y, z))
+    
+    if message.content.startswith("!functions") and check:
         commands = ""
         for i in commandlist:
-            commands += (i + "," + " ")
+            commands += ("```\n" + i.name + "\n\n" + i.desc + "\n" + "\n" + "\n" + i.functions + '```' )
+            
 
         await message.channel.send(commands)
         
 
 
     if message.content == "hello" and check:
-        await message.channel.send('wassup')
+        await messagefunc(client, message.channel, "hi")
 
 
 
